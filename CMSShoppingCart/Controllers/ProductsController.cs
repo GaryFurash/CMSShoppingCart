ab99179167849a98cf969d8de4373e2020c80a9b
GaryFurash <furashgary@gmail.com>
2020-02-26 07:11:11 -0700

    added pagination for products by category pages

--- CMSShoppingCart/Controllers/ProductsController.cs
+++ CMSShoppingCart/Controllers/ProductsController.cs
@@ -42,7 +42,7 @@ namespace CMSShoppingCart.Controllers
 		public async Task<IActionResult> ProductsByCategory(string categorySlug, int p = 1)
 		{
 			Category category = await context.Categories.Where(x => x.Slug == categorySlug).FirstOrDefaultAsync();
-			if (category == null) RedirectToAction("Index");
+			if (category == null) return RedirectToAction("Index");
 
 			int pageSize = 6;
 			var products = context.Products.OrderByDescending(x => x.Id)
