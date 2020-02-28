be760092e44794d513a823117adf1776caace21a
GaryFurash <furashgary@gmail.com>
2020-02-28 06:41:16 -0700

    Added SessionExtensions

---
+++ CMSShoppingCart/Controllers/CartController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMSShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMSShoppingCart.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly CmsShoppingCartContext cmsShoppingCartContext;
+
+        public CartController(CmsShoppingCartContext cmsShoppingCartContext)
+        {
+            this.cmsShoppingCartContext = cmsShoppingCartContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CMSShoppingCart/Infrastructure/SessionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSShoppingCart.Infrastructure
+{
+	public static class SessionExtensions
+	{
+		// this ... sets up this method as an extension method. in this example
+		// using this ISession as the first parameter means that you'll be able
+		// to use the syntax [ISession Object].SetJson as if SetJson was a method
+		// for ISession
+		public static void SetJson(this ISession session, string key, object value)
+		{
+			session.SetString(key, JsonConvert.SerializeObject(value));
+		}
+
+		public static T GetJson<T>(this ISession session, string key)
+		{
+			var sessionData = session.GetString(key);
+			return sessionData == null ? default(T) : JsonConvert.DeserializeObject<T>(sessionData);
+		}
+	}
+}
