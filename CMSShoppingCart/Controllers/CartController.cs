c62a22e8bc453662463e579edc9b959deb19b94e
GaryFurash <furashgary@gmail.com>
2020-02-28 14:41:44 -0700

    added cart model items

--- CMSShoppingCart/Controllers/CartController.cs
+++ CMSShoppingCart/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CMSShoppingCart.Infrastructure;
+using CMSShoppingCart.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CMSShoppingCart.Controllers
@@ -17,7 +18,16 @@ namespace CMSShoppingCart.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+            // ViewModel - non database (DTO) object used as backing for a view
+            CartViewModel cartVM = new CartViewModel
+            {
+                CartItems = cart,
+                GrandTotal = cart.Sum(x => x.Price * x.Quantity)
+            };
+
+            return View(cartVM);
         }
     }
 }
