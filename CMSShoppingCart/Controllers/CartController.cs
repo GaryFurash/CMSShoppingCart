6c488f0c67b20c1535feb95a9a108d05fec2dd5a
GaryFurash <furashgary@gmail.com>
2020-03-12 06:37:52 -0700

    added small cart partial view

--- CMSShoppingCart/Controllers/CartController.cs
+++ CMSShoppingCart/Controllers/CartController.cs
@@ -8,52 +8,113 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CMSShoppingCart.Controllers
 {
-    public class CartController : Controller
-    {
-        private readonly CmsShoppingCartContext context;
+	public class CartController : Controller
+	{
+		private readonly CmsShoppingCartContext context;
 
-        public CartController(CmsShoppingCartContext cmsShoppingCartContext)
-        {
-            context = cmsShoppingCartContext;
-        }
+		public CartController(CmsShoppingCartContext cmsShoppingCartContext)
+		{
+			context = cmsShoppingCartContext;
+		}
 
-        // GET /cart
-        public IActionResult Index()
-        {
-            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+		// GET /cart
+		public IActionResult Index()
+		{
+			List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
 
-            // ViewModel - non database (DTO) object used as backing for a view
-            CartViewModel cartVM = new CartViewModel
-            {
-                CartItems = cart,
-                GrandTotal = cart.Sum(x => x.Price * x.Quantity)
-            };
+			// ViewModel - non database (DTO) object used as backing for a view
+			CartViewModel cartVM = new CartViewModel
+			{
+				CartItems = cart,
+				GrandTotal = cart.Sum(x => x.Price * x.Quantity)
+			};
 
-            return View(cartVM);
-        }        
-        
-        // GET /cart/add/5
-        public async Task<IActionResult> Add(int id)
-        {
-            Product product = await context.Products.FindAsync(id);
+			return View(cartVM);
+		}
 
-            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+		// GET /cart/add/5
+		public async Task<IActionResult> Add(int id)
+		{
+			Product product = await context.Products.FindAsync(id);
 
-            CartItem cartItem = cart.Where(x => x.ProductId == id).FirstOrDefault();
+			List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
 
-            if (cartItem == null)
-            {
-                cart.Add(new CartItem(product));
-            }
-            else
-            {
-                cartItem.Quantity += 1;
-            }
+			CartItem cartItem = cart.Where(x => x.ProductId == id).FirstOrDefault();
 
-            // use SetInt(32) or SetString for simpler types
-            HttpContext.Session.SetJson("Cart", cart);
+			if (cartItem == null)
+			{
+				cart.Add(new CartItem(product));
+			}
+			else
+			{
+				cartItem.Quantity += 1;
+			}
 
-            return RedirectToAction("Index");
-        }
-    }
+			// use SetInt(32) or SetString for simpler types
+			HttpContext.Session.SetJson("Cart", cart);
+
+			return RedirectToAction("Index");
+		}
+
+		// GET /cart/decrease/5
+		public IActionResult Decrease(int id)
+		{
+			List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+			CartItem cartItem = cart.Where(x => x.ProductId == id).FirstOrDefault();
+
+			if (cartItem.Quantity > 1)
+			{
+				--cartItem.Quantity;
+			}
+			else
+			{
+				cart.RemoveAll(x => x.ProductId == id);
+			}
+
+			// update cart
+
+			// clear cart if no remaining items
+			if (cart.Count == 0)
+			{
+				HttpContext.Session.Remove("Cart");
+			}
+			else
+			{
+				HttpContext.Session.SetJson("Cart", cart);
+			}
+
+			return RedirectToAction("Index");
+		}
+
+		// GET /cart/remove/id
+		public IActionResult Remove(int id)
+		{
+			List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+			cart.RemoveAll(x => x.ProductId == id);
+
+			// update cart
+
+			// clear cart if no remaining items
+			if (cart.Count == 0)
+			{
+				HttpContext.Session.Remove("Cart");
+			}
+			else
+			{
+				HttpContext.Session.SetJson("Cart", cart);
+			}
+
+			return RedirectToAction("Index");
+		}
+
+		// GET /cart/clear
+		public IActionResult Clear()
+		{
+			HttpContext.Session.Remove("Cart");
+
+			return RedirectToAction("Index");
+		}
+	}
 }
